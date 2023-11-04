5bdc464e3cbdf50d5ba1fff3c82c1990468dc044
Bereczki Attila <bereczki.at@gmail.com>
2023-11-04 21:25:57 +0100

    First

---
+++ src/main/java/hu/example/samplerecursiveapp/SampleRecursiveAppApplication.java
@@ -0,0 +1,13 @@
+package hu.example.samplerecursiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRecursiveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRecursiveAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/example/samplerecursiveapp/SampleRecursiveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.example.samplerecursiveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRecursiveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
